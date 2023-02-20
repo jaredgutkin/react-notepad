007a1bba229d8a16ddab8b7edf11292b62b0f006
Jared Gutkin <jgutkinsam@gmail.com>
2023-02-19 21:03:41 -0600

    notes page started

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import {CiSearch} from 'react-icons/ci'
 
 function Notes() {
   return (
-    <div>Notes</div>
+    <section>
+        <header className="notes__header">
+            <h2>My Notes</h2>
+            <input type="text" autoFocus placeholder='Keyword...' />
+            <button className='btn'><CiSearch/></button>
+        </header>
+    </section>
   )
 }
 
