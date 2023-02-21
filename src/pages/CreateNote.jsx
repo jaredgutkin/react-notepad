f821c64af0a52493acc007658d12e500acda2041
Jared Gutkin <jgutkinsam@gmail.com>
2023-02-20 18:54:12 -0600

    create note page added

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom"
+import { IoIosArrowBack } from 'react-icons/io'
 
 function CreateNote() {
   return (
-    <div>CreateNote</div>
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className="btn"><IoIosArrowBack/></Link>
+        <button className="btn lg primary">Save</button>
+      </header>
+      <form className="create-note__form">
+        <input type="text" placeholder="Title" autoFocus />
+        <textarea rows="28" placeholder="Note details..."></textarea>
+      </form>
+    </section>
   )
 }
 
