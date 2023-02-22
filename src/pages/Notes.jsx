7779cd62708555a8fa52a34f0cda2e707987c3ef
Jared Gutkin <jgutkinsam@gmail.com>
2023-02-21 20:10:00 -0600

    final touchups added

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -42,6 +42,7 @@ const Notes = ({notes}) => {
             </button>
         </header>
         <div className="notes__container">
+          {filteredNotes.length == 0 && <p className='empty__notes'>No Notes Found.</p>}
           {
             filteredNotes.map(note => <NoteItem key={note.id} note={note} />)
           }    
