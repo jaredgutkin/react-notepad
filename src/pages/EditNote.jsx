94feeeaf656fdfc3568c6007d8e0e30c2b833bb5
Jared Gutkin <jgutkinsam@gmail.com>
2023-02-21 19:28:45 -0600

    edit functionality added

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,23 +1,46 @@
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams, useNavigate } from "react-router-dom"
 import { IoIosArrowBack } from 'react-icons/io'
 import { RiDeleteBin6Line } from 'react-icons/ri'
 
 import { useState } from "react";
+import useCreateDate from "../components/useCreateDate";
 
 function EditNote({notes, setNotes}) {
   const {id} = useParams();
   const note = notes.find((item) => item.id == id);
   const [title, setTitle] = useState(note.title)
   const [details, setDetails] = useState(note.details)
+  const date = useCreateDate();
+  const navigate = useNavigate();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    if(title && details) {
+      const newNote = {...note, title, details, date}
+
+      const newNotes = notes.map(item => {
+        if(item.id == id) {
+          item = newNote;
+        }
+        return item;
+      })
+
+      setNotes(newNotes);
+    }
+
+    //redirect to home
+    navigate('/')
+  }
 
   return (
     <section>
       <header className="create-note__header">
         <Link to="/" className="btn"><IoIosArrowBack/></Link>
-        <button className="btn lg primary">Save</button>
+        <button className="btn lg primary" onClick={handleForm}>Save</button>
         <button className="btn danger"><RiDeleteBin6Line/></button>
       </header>
-      <form className="create-note__form">
+      <form className="create-note__form" onSubmit={handleForm}>
         <input 
           type="text" 
           placeholder="Title" 
